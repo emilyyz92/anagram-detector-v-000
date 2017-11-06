824b9260aa6a46da02b47c184f90b0e9224185af
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-06 00:11:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,5 +4,11 @@ class Anagram
   def initialize(new_word)
     @word = new_word
   end
-  
+
+  def match(array)
+    array.each do |word_comp|
+      if self.word.sort == word_comp.sort
+        word_comp
+      end
+    end
 end
